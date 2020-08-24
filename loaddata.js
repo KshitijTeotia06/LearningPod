9a326fbe7fdfda59cfd9a07c5e56085325c6b45d
Kshitij T <teotia.kshitij@gmail.com>
2020-08-23 17:10:09 -0700

    added new lobby functions

--- loaddata.js
+++ loaddata.js
@@ -11,15 +11,33 @@ firebase.database().ref(localStorage.getItem("uid") + "/lobby").once('value').th
             console.log("PARENT NODE: " + child.val())
             var count = 0;
             url2Ref.once("value", function(snappy) {
+                var inar = [];
                 snappy.forEach(function(child) {
-                    if(count !=2 && count != 3){
-                        array.splice(parentcount, count, child.val());
-                        console.log(child.val());
-                    }
+                    array.splice(parentcount, count, child.val());
+                    inar.push(child.val());
                     count++;
                 });
+                console.log("ARRAY SADFASDF: " + inar);
+                array.push(inar);
+                let table = document.createElement('table');
+                //iterate over every array(row) within tableArr
+                for (let row of array) {
+                //Insert a new row element into the table element
+                table.insertRow();
+                //Iterate over every index(cell) in each array(row)
+                for (let cell of row) {
+                //While iterating over the index(cell)
+                //insert a cell into the table element
+                    let newCell = table.rows[table.rows.length - 1].insertCell();
+                //add text to the created cell element
+                    newCell.textContent = cell;
+                }
+                }
+                //append the compiled table to the DOM
+                document.body.appendChild(table);
             });
         });
         parentcount++;
     });
 });
+

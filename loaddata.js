eb8e6d700aa8a589c53697b312405c2da48287b9
Kshitij T <teotia.kshitij@gmail.com>
2020-08-27 21:20:26 -0700

    Added new table with auto updating information in display pod

--- loaddata.js
+++ loaddata.js
@@ -1,36 +1,47 @@
-var array = [];
-var lobby;
-firebase.database().ref(localStorage.getItem("uid") + "/lobby").once('value').then(function(snapshot) { 
-    var rootRef = firebase.database().ref();
-    var urlRef = rootRef.child("Lobby/"+snapshot.val());
-    urlRef.once("value", function(snappy) {
-        var parentcount = 0;
-        snappy.forEach(function(child) {
-            var root2Ref = firebase.database().ref();
-            var url2Ref = root2Ref.child(child.val());
-            console.log("PARENT NODE: " + child.val())
-            var count = 0;
-            url2Ref.once("value", function(snappy) {
-                var inar = [];
-                snappy.forEach(function(child) {
-                    array.splice(parentcount, count, child.val());
-                    inar.push(child.val());
-                    count++;
-                });
-                console.log("ARRAY SADFASDF: " + inar);
-                array.push(inar);
-                let table = document.createElement('table');
-                for (let row of array) {
-                table.insertRow();
-                for (let cell of row) {
-                    let newCell = table.rows[table.rows.length - 1].insertCell();
-                    newCell.textContent = cell;
-                }
-                }
-                document.body.appendChild(table);
-            });
-        });
-        parentcount++;
-    });
+firebase.database().ref("Lobby/" ).on('value', function(snap){
+    console.log(snap.val());
+    console.log(snap.numChildren());
 });
 
+var tableRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+
+firebase.database().ref(localStorage.getItem("uid")).once('value', function(snap){
+    var data = snap.val();
+    firebase.database().ref("Lobby/" + data["lobby"]).on('value', function(snap2){
+
+        var table = document.getElementById('myTable');
+        for(var i = table.rows.length - 1; i > 0; i--)
+        {
+            table.deleteRow(i);
+        }
+
+        var people = snap2.val();
+        var keys = Object.keys(people);
+        for(var i = 0; i<keys.length; i++){
+            var k = keys[i];
+            var getuid = people[k];
+            console.log(getuid);
+            firebase.database().ref(getuid).once('value', function(snap3){
+                var getall =snap3.val();
+
+                var newRow   = tableRef.insertRow();
+
+                // Insert a cell in the row at index 0
+                var newCell  = newRow.insertCell(0);
+
+                // Append a text node to the cell
+                var newText  = document.createTextNode(getall["name"]);
+                newCell.appendChild(newText);
+
+                var newCell2 = newRow.insertCell(1);
+                var newText2 = document.createTextNode(getall["school"]);
+                newCell2.append(newText2);
+
+                var newCell3 = newRow.insertCell(2);
+                var newText3 = document.createTextNode(getall["email"]);
+                newCell3.append(newText3);
+
+            })
+        }
+    });
+})
